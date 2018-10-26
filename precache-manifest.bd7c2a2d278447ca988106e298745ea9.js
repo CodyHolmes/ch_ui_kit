6e9fc29c692f40f40481a97cc153727a63899f15
Cody Holmes <codyholmes0@gmail.com>
2018-10-26 16:58:12 -0400

    Updates

---
+++ precache-manifest.bd7c2a2d278447ca988106e298745ea9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1e365e3e799dfbf2e84",
+    "url": "/ch_ui_kit/static/js/runtime~main.b1e365e3.js"
+  },
+  {
+    "revision": "9d70a5461f9f061ce4b1",
+    "url": "/ch_ui_kit/static/js/main.9d70a546.chunk.js"
+  },
+  {
+    "revision": "04dde2be9f570c90f732",
+    "url": "/ch_ui_kit/static/js/1.04dde2be.chunk.js"
+  },
+  {
+    "revision": "9d70a5461f9f061ce4b1",
+    "url": "/ch_ui_kit/static/css/main.702c5cb1.chunk.css"
+  },
+  {
+    "revision": "850afe4b40f0ae2df211dd0645da1576",
+    "url": "/ch_ui_kit/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/ch_ui_kit/precache-manifest.aa99ea340ee7fd9367f88b4ca09e36f0.js"
+  "/ch_ui_kit/precache-manifest.bd7c2a2d278447ca988106e298745ea9.js"
 );
 
 workbox.clientsClaim();
